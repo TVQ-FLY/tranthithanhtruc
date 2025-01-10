ebf23cd9daf5532cb0d7042fb98e62d2dfc801b4
unknown <tranvanquyenx4@gmail.com>
2025-01-10 19:58:37 +0700

    fix

--- script.js
+++ script.js
@@ -64,26 +64,26 @@ function scrollToTop() {
   });
 }
 
-// Lắng nghe sự kiện 'contextmenu' trên toàn bộ trang
-document.addEventListener("contextmenu", function (event) {
-  // Ngăn chặn menu chuột phải mặc định
-  event.preventDefault();
-  // Hiển thị thông báo
-  alert(
-    "Xin chào ! \nHành động của bạn bị vô hiệu hóa !\n- Sent by Trần Văn Quyền -"
-  );
-});
+// // Lắng nghe sự kiện 'contextmenu' trên toàn bộ trang
+// document.addEventListener("contextmenu", function (event) {
+//   // Ngăn chặn menu chuột phải mặc định
+//   event.preventDefault();
+//   // Hiển thị thông báo
+//   alert(
+//     "Xin chào ! \nHành động của bạn bị vô hiệu hóa !\n- Sent by Trần Văn Quyền -"
+//   );
+// });
 
-// Lắng nghe sự kiện 'keydown' để kiểm tra khi người dùng nhấn phím F12
-document.addEventListener("keydown", function (event) {
-  // Kiểm tra nếu phím F12 (mã phím 123) được nhấn
-  if (event.key === "F12" || event.keyCode === 123) {
-    event.preventDefault();
-    alert(
-      "Xin Chào ! \nBạn không thể mở DevTools bằng F12 ! \n- Sent by Trần Văn Quyền -"
-    );
-  }
-});
+// // Lắng nghe sự kiện 'keydown' để kiểm tra khi người dùng nhấn phím F12
+// document.addEventListener("keydown", function (event) {
+//   // Kiểm tra nếu phím F12 (mã phím 123) được nhấn
+//   if (event.key === "F12" || event.keyCode === 123) {
+//     event.preventDefault();
+//     alert(
+//       "Xin Chào ! \nBạn không thể mở DevTools bằng F12 ! \n- Sent by Trần Văn Quyền -"
+//     );
+//   }
+// });
 
 // // Khởi tạo biến để theo dõi thời gian giữ chuột
 // let mouseHoldTimeout;
